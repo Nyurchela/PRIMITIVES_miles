73fd23c04ce14a54bb0583ea151dfabef8aafc55
Анна Лаврентьева <anncom@bk.ru>
2022-07-15 21:22:44 +0300

    Расчет бонусных миль

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2000;
+        int oneMile = 20;
+        int bonusMiles = ticketPrice/oneMile;
+
+        System.out.println("Вам начислено " + bonusMiles + " бонусных миль!");
+
+    }
+}
