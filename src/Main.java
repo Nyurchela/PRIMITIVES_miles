5cb416dbc03d753189e4504d6dd1ee237c8132e0
Анна Лаврентьева <anncom@bk.ru>
2022-07-18 20:34:49 +0300

    Рефракторинг кода

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int ticketPrice = 2000;
         int oneMile = 20;
-        int bonusMiles = ticketPrice/oneMile;
+        int bonusMiles = ticketPrice / oneMile;
 
         System.out.println("Вам начислено " + bonusMiles + " бонусных миль!");
 
